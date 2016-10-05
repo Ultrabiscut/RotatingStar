5fe9b0724b03039cfaaebb9ca87f1d752a2c7548
Tyler Timmins <timmins_tyler@yahoo.com>
2016-10-05 16:01:05 -0500

    Add files via upload

---
+++ RotatingStar/OpenGL/OpenGLApp.cpp
@@ -0,0 +1,189 @@
+#include<Windows.h>
+#include<gl\GL.h>
+#include<gl\GLU.h>
+#include <cmath>
+#include "OpenGLApp.h"
+
+
+OpenGLApp::OpenGLApp()
+{
+}
+
+
+OpenGLApp::~OpenGLApp()
+{
+}
+
+
+bool OpenGLApp::Initialize()	//initialize your opengl scene
+{
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	return true;
+}	
+bool OpenGLApp::Shutdown()	//shut down your scene
+{
+	return true;
+}
+
+//used to setup your opengl camera and drawing area
+void OpenGLApp::SetupProjection(int width, int height){
+	if (height == 0)
+	{
+		height = 1;
+	}
+
+	glViewport(0, 0, width, height);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+	// you need 3 things to create a frustum
+	// 1. Field of view (52.0f here)
+	// 2. aspect ratio of screen (width/height of window)
+	// 3. near and far plane (1, and 1000)
+	gluPerspective(60.0f, (GLfloat)width / (GLfloat)height, 1.0f, 1000.0f);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	m_windowWidth = width;
+	m_windowHeight = height;
+}
+
+void OpenGLApp::Prepare(float dt)
+{
+
+}
+void OpenGLApp::Render()
+{
+	static float angle = 0.0f;
+	static float red = 0.0f, blue = 0.0f, green = 0.0f;
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+
+	glLoadIdentity();
+
+	glTranslatef(0.0f, 0.0f, -5.0f);
+
+	glColor3f(0.73f, 0.16f, 0.96f); //purple
+
+	//gluLookAt - move the camera and change the 
+	// direction it is looking
+
+	//syntax for gluLookAt:
+	//gluLookAt  (x, y, z //location of camera
+	//			  x, y, z //where it's looking
+	//			  x, y, z) //which way is up
+	
+	//gluLookAt(0.0f, 0.0f, 0.1,
+	//		  0.0f, 0.0f, 0.0f,
+	//	      0.0f, 1.0f, 0.0f);
+
+	//set polymode - on the front draw lines only
+	glPolygonMode(GL_FRONT, GL_LINE);
+	glPushMatrix(); //save current model view settings (no rot, no scale, no translate)
+		glTranslatef(0.0f, 0.5f, 0.0f);
+		glRotatef(angle, 0.0f, 0.0f, 1.0f);
+		glColor3f(red, green, blue); //flash
+		DrawStar();
+	glPopMatrix(); //restore the saved modelview settings (no rot, no scale, no translate)
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL); //restore default polymode
+	
+	//make this funciton call if you want to see the grid
+	DrawGrid();
+
+	//increase the angle
+	angle += 1.0f;
+
+	red += 0.1f;
+	green += 0.2f;
+	blue += 0.3f;
+
+	if (red > 1.0f) red = 0;
+	if (green > 1.0f) green = 0;
+	if (blue > 1.0f) blue = 0;
+
+}
+
+void OpenGLApp::DrawGrid()
+{
+	//increase the size of drawn points
+	glPointSize(3.0f);
+
+	glBegin(GL_LINES);
+	//change the color to blue
+	glColor3f(0.0f, 0.0, 1.0f);
+
+	//vertical
+	for (float x = 8; x >= -8; x--)
+	{
+		glVertex3f(x, 5.0f, 0.0f);
+		glVertex3f(x, -5.0f, 0.0f);
+	}
+	//horizontal
+	for (float y = 5; y >= -5; y--)
+	{
+		glVertex3f(8.0f, y, 0.0f);
+		glVertex3f(-8.0f, y, 0.0f);
+	}
+
+	glEnd();
+	
+	//we are going to draw below
+	glBegin(GL_POINTS);
+
+		//change color to white
+		glColor3f(1.0f, 1.0f, 1.0f);
+
+		//draw the points on the y-axis
+		for (float y = 5; y >= -5; y--)
+		{
+			glVertex3f(0.0f, y, 0.0f);
+		}
+
+		//draw the points on the x-axis
+		for (float x = 8; x >= -8; x--)
+		{
+			glVertex3f(x, 0.0, 0.0f);
+		}
+
+		//set the color to red
+		glColor3f(1.0f, 0.0f, 0.0f);
+		//draw the orgin
+		glVertex3f(0.0f, 0.0f, 0.0f);
+
+	glEnd();
+
+}
+
+void OpenGLApp::Draw2DSquare()
+{
+	
+	glBegin(GL_POLYGON);
+		glVertex3f(-0.5f, 0.0f, 0.0f); // (x,y,z)
+			//glColor3f(0.0f, 1.0f, 0.0f); //blue
+		glVertex3f(0.5f, 0.0f, 0.0f);
+			//glColor3f(1.0f, 0.0f, 0.0f); //red
+		glVertex3f(0.5f, 0.0f, -1.0f);
+			//glColor3f(0.0f, 0.0f, 1.0f); //green
+		glVertex3f(-0.5f, 0.0f, -1.0f);
+		
+	glEnd();
+}
+
+void OpenGLApp::DrawStar()
+{
+	glBegin(GL_LINE_LOOP);
+
+	glVertex3f(0.0, 0.7, 0.0);
+	glVertex3f(-0.3, 0.0, 0.0);
+	glVertex3f(-1.0, 0.0, 0.0);
+	glVertex3f(-0.4, -0.5, 0.0);
+	glVertex3f(-0.7, -1.2, 0.0);
+	glVertex3f(0.0, -0.8, 0.0);
+	glVertex3f(0.7, -1.2, 0.0);
+	glVertex3f(0.4, -0.5, 0.0);
+	glVertex3f(1.0, 0.0, 0.0);
+	glVertex3f(0.3, 0.0, 0.0);
+
+	glEnd();
+}
